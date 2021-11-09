09562c06712a28a3ed802147028781622535587f
von1987 <78596274+von1987@users.noreply.github.com>
2021-11-09 19:24:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,24 +14,26 @@ provider "cloudstack" {
   secret_key = var.cloudstack_secret_key
 }
 
+resource "cloudstack_instance" "web" {
+  name             = ""
+  service_offering = "t3.medium"
+  network_id       =  cloudstack_network.NewEthernet.id
+  template         = "54c34c71-0bf9-40d7-a092-0abe4331ffc6"
+  zone             = "cc680757-4e70-4609-8c2f-c8ccae36dff6"
+  start_vm         = "true"
+  keypair          = "myKey"
+  expunge          = "true"
+  depends_on       = [
+    cloudstack_network.NewEthernet
+  ]
+}
+
 resource "cloudstack_network" "NewEthernet" {
   name             = "NewEthernet"
   cidr             = "10.4.12.0/24"
   network_offering = "DefaultIsolatedNetworkOfferingWithSourceNatService"
   zone             = "SOF-1"
   source_nat_ip    = "true"
-}
-
-resource "cloudstack_instance" "web" {
-  name             = ""
-  service_offering = "t3.medium"
-  network_id       = "a4ced0c4-8cce-4919-9b33-ec2a24811588"
-  template         = "54c34c71-0bf9-40d7-a092-0abe4331ffc6"
-  zone             = "cc680757-4e70-4609-8c2f-c8ccae36dff6"
-  start_vm         = "true"
-  keypair          = "myKey"
-  expunge          = "true"
-
 }
 
 resource "cloudstack_firewall" "MyNewFirewallRulles" {
@@ -48,7 +50,7 @@ resource "cloudstack_firewall" "MyNewFirewallRulles" {
 }
 
 resource "cloudstack_egress_firewall" "EgressFirewall" {
-  network_id = "a4ced0c4-8cce-4919-9b33-ec2a24811588"
+  network_id = cloudstack_network.NewEthernet.id
 
   rule {
     cidr_list = ["10.4.12.0/24"]
@@ -76,4 +78,3 @@ resource "cloudstack_port_forward" "MyNewPortForward" {
       cloudstack_instance.web, 
     ]
 }
-
